4e186bb8de9e60e060f370ef9137ee336921f7c1
musa sarıyar <musa.sariyar@tsoft.com.tr>
2020-02-24 20:07:26 +0300

    lastt commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_burc_rehberi/burc_liste.dart';
+import 'package:flutter_burc_rehberi/testlib.dart';
 
-void main() => runApp(MyApp());
+import 'burc_detay.dart';
 
+void main() => runApp(MyApps());
 
-class MyApp extends StatelessWidget {
+class MyApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Burc Rehberi",
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/burcListesi",
+      routes: {
+        "/": (context) => BurcListesi(),
+        "/burcListesi": (context) => BurcListesi(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        List<String> pathElemanlari =
+            settings.name.split("/"); //  /  burcDetay   /   1
+        if (pathElemanlari[1] == 'burcDetay') {
+          return MaterialPageRoute(
+              builder: (context) => BurcDetay(int.parse(pathElemanlari[2])));
+        }
+        return null;
+      },
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-
-      home: BurcListesi(),
     );
   }
-
 }
